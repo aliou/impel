ea9788a6f46271663ec0fa6ab9b508063d219e82
Aliou Diallo <code@aliou.me>
2020-02-09 09:15:04 +0100

    Let bash handle the prompt char.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ mod impel;
 
 fn main() {
     println!(
-        "{hostname}: {cwd} {vcs}{vim}{pchar} ",
+        // "{hostname}: {cwd} {vcs}{vim}{pchar} ",
+        "{hostname}: {cwd} {vcs}{vim}",
         hostname = impel::hostname().color("red").bold(),
         cwd = impel::working_directory(),
         vcs = impel::vcs().color("blue").bold(),
         vim = impel::vim(),
-        pchar = impel::pchar()
+        // pchar = impel::pchar()
     );
 }
